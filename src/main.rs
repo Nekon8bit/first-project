78e2c891a12de39f4452c483bc467543200e9898
Nekon8bit <nekonsantos@gmail.com>
2026-09-06 09:49:15 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    greetings("murilow");
+    greetings("bidu");
+    greetings("vitória");
+    greetings("mauricio de souza");
+    greetings("emily");
+}
+
+fn greetings(name: &str) {
+    println!("{}", name); 
+}
